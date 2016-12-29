d3fd96e6fe4de29c4c1ff8d6a0b722259e70ac0b
gvitalii1993 <gvitalii1993@gmail.com>
2016-12-29 17:11:49 +0200

    Revert "Revert "sdse""
    This reverts commit 1f8b44983434be08cf72c6f29bc9f44a3acc2e66.

--- Сhaos-Сhronicles/js/script.js
+++ Сhaos-Сhronicles/js/script.js
@@ -7,7 +7,7 @@ function popupclose(){
 		var popUp =  document.querySelector(".popUp");
 		popUp.style.display = "none";
         
-        var container = document.getElementById("content");
+        var content = document.getElementById("content");
         // var newDiv = document.createElement('div');
         var inputName = document.getElementById("InputNamePlaer").value;
         var InputVkPlaer = document.getElementById("InputVkPlaer").value;
@@ -18,19 +18,26 @@ function popupclose(){
 		var d = date.getDate();
 		var resultDate = d+"."+(m+1)+"."+y;
 
+		var InputLvlPlaer = document.getElementById("InputLvlPlaer").value;
+
+
+		var selected = document.getElementById("mySelect");
+		var selectedindex = selected.selectedIndex;
+		var selectedWhere = selected.options[selectedindex].innerHTML;
+
 		var Html = 	'<div id="plaer-1" class="col-md-12">' +
 						'<div class="avatar-name-1">' +
 							'<div class="avatar-1"></div>' +
 								'<div class="name-1">' +
-									"<h3>"+ inputName +"</h3>" +
-									"<p>Уровень 80</p>" +
+									"<h3>"+inputName+"</h3>" +
+									"<p>Уровень "+InputLvlPlaer+"</p>" +
 								"</div>" +
 							'</div>' +
 						'<div class="data-rang-1">' +
-							"<p>Хранитель</p>" +
+							"<p>"+selectedWhere+"</p>" +
 							'<div class="date-1">' +
-								'<a href="https://vk.com/id228642634" target="_blank"><i class="fa fa-vk" aria-hidden="true"></i></a>' +
-								'<p>'+ resultDate +'</p>' +
+								'<a href="'+InputVkPlaer+'" target="_blank"><i class="fa fa-vk" aria-hidden="true"></i></a>' +
+								'<p>'+resultDate+'</p>' +
 							'</div>' +							
 							'<button></button>' +
 						'</div>' +
@@ -41,6 +48,6 @@ function popupclose(){
 					'</div>';
 
         // newDiv.id = 'plaer-1';
-        container.insertAdjacentHTML("beforeend", Html);
+        content.insertAdjacentHTML("beforeend", Html);
         // container.appendChild(newDiv);
 };
