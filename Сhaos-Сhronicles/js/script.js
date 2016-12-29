d353e80ae6afca744c1637333f23419ed1bb8ecb
gvitalii1993 <gvitalii1993@gmail.com>
2016-12-29 03:25:54 +0200

    input popUp

--- Сhaos-Сhronicles/js/script.js
+++ Сhaos-Сhronicles/js/script.js
@@ -1,18 +1,22 @@
 function popup(){
 		var popUp =  document.querySelector(".popUp");
 		popUp.style.cssText = "display: flex;";
-	}
+	};
+
 function popupclose(){
 		var popUp =  document.querySelector(".popUp");
 		popUp.style.display = "none";
         
         var container = document.getElementById("content");
-        // var newDiv = document.createElement('div'); 
+        // var newDiv = document.createElement('div');
+        var inputName = document.getElementById("InputNamePlaer").value;
+        var InputVkPlaer = document.getElementById("InputVkPlaer").value;
+
 		var Html = 	'<div id="plaer-1" class="col-md-12">' +
 						'<div class="avatar-name-1">' +
 							'<div class="avatar-1"></div>' +
 								'<div class="name-1">' +
-									"<h3>WaPaW</h3>" +
+									"<h3>"+ inputName +"</h3>" +
 									"<p>Уровень 80</p>" +
 								"</div>" +
 							"</div>" +
@@ -30,4 +34,4 @@ function popupclose(){
         // newDiv.id = 'plaer-1';
         container.insertAdjacentHTML("beforeend", Html);
         // container.appendChild(newDiv);
-	}
+	};
