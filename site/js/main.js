b5dd3276ff482bfebcbf97e0c3adce1fffdb88eb
vetal <gvitalii1993@gmail.com>
2018-02-26 01:32:12 +0200

    26.02

--- site/js/main.js
+++ site/js/main.js
@@ -1,13 +1,13 @@
 		$(document).ready(function() {
-			$('.nav-button').click( function(event){
+			$('.nav_button').click( function(event){
 
-				if ($(".nav-bar").css("left") == "0px") {
-        			$(".nav-bar").css("left","-20%");
-        			$(".nav-button").css("transform","scale(-1, 1)");
+				if ($(".nav_bar").css("left") == "0px") {
+        			$(".nav_bar").css("left","-20%");
+        			$(".nav_button").css("transform","scale(-1, 1)");
     			}
     			else {
-    				$(".nav-bar").css("left","0px")
-    				$(".nav-button").css("transform","scale(1, -1)");
+    				$(".nav_bar").css("left","0px")
+    				$(".nav_button").css("transform","scale(1, -1)");
     			}  
 			});
 		});
--- site/php/index.php
+++ site/php/index.php
@@ -1,36 +1,12 @@
-<?php  
-require "php/db.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="style/bootstrap.css">
-	<link rel="stylesheet" href="style/style.css">
-	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script src="js/main.js"></script>
-</head>
-<body>
-	<div class="container-s">
-		<div class="nav-bar col-md-2">
-			<div class="nav-button"></div>
-			<div class="login-to-home">
-				<a href="php/signup.php">регистрация</a>
-			</div>
-		</div>
-
-		<div class="content">
-			<div class="col-md-12 dir" >
-				<?php 
-				echo date ('d.m.Y.');
-
-			 ?>
-			</div>
-		</div>
-		
+<?php
+<div class="nav_bar_user_profile">
+	<div class="nav_bar_user_profile_avatar">
+		<img src="img/ava.jpeg" alt="avatar">
+		<img src="img/backgraund_ava.png" class="nav_bar_user_profile_avatar_backgraund" alt="ava_back">
+	</div>
+	<div class="nav_bar_user_profile_name col-xs-12 col-lg-10">
+		<p>Paulo Picasso</p>
 	</div>
 	
-</body>
-</html>
-
+</div>	
+?>
